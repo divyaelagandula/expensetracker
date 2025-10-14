6b2279096ccefc23b6e61e1a18698382cdd6cdf5
divyaelagandula <divyaelagandula9014@gmail.com>
2025-10-14 13:09:56 +0530

    login

--- vpractice/controllers/usercontroller.js
+++ vpractice/controllers/usercontroller.js
@@ -4,10 +4,10 @@ function isnotvalid(string){
         return true
     }
 }
-const addUserInformation=async (req,res)=>{
+const signup=async (req,res)=>{
     try{
         const {name,email,password}=req.body
-        console.log('name',name)
+       
         if(isnotvalid(name)||isnotvalid(email)||isnotvalid(password)){
             return res.status(400).json({Error:'missing input filed'})
         }
@@ -26,6 +26,41 @@ const addUserInformation=async (req,res)=>{
 
 
 }
+const login=async (req,res)=>{
+    try{
+        console.log(req.body)
+        const {email,password}=req.body
+        if(isnotvalid(email)||isnotvalid(password)){
+            return res.status(400).json({Error:'missing input filed'})
+        }
+        const listOfUsers=await users.findAll({raw:true})
+        console.log(listOfUsers)
+        const userEmailChecking=listOfUsers.find(user=>user.email==email)
+        const userPasswordChecking=listOfUsers.find(user=>user.password==password)
+        console.log('user email',userEmailChecking)
+        console.log('user paswoord',userPasswordChecking)
+        if(userEmailChecking && userPasswordChecking){
+            return res.status(200).json({succeses:true,message:'user logged successfully'})
+        }
+        if(!userEmailChecking){
+             return res.status(404).json({succeses:false,message:'user not found'})
+        }
+        if(!userPasswordChecking){
+             return res.status(400).json({succeses:false,message:'password invalid'})
+        }
+
+        
+
+
+
+    }
+    catch(error){
+        console.log(error)
+        res.status(500).send('error while login')
+    }
+
+}
 module.exports={
-    addUserInformation
+    signup,
+    login
 }
