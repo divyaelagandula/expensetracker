e081db54d20a5715af80ee56da831a5865c1b55a
divyaelagandula <divyaelagandula9014@gmail.com>
2025-10-20 20:52:44 +0530

    Leaderboard Optimisation

--- vpractice/controllers/premiumcontroller.js
+++ vpractice/controllers/premiumcontroller.js
@@ -1,11 +1,11 @@
 const {Sequelize}=require('sequelize')
-const userModel = require('../models/users');
+const users = require('../models/users');
 const expenses=require('../models/expenses') // Use a descriptive variable name
 
 const checkingUserIsPremiumOrNot = async (req, res) => {
     try {
         // Renaming the result variable to userResult
-        const userResult = await userModel.findByPk(req.user.id, { raw: true }); 
+        const userResult = await users.findByPk(req.user.id, { raw: true }); 
         
         console.log('Checking premium status:', userResult);
 
@@ -20,31 +20,24 @@ const checkingUserIsPremiumOrNot = async (req, res) => {
 };
 const leaderBoard=async (req,res)=>{
     try{
-        const userexpenses=await expenses.findAll({
-        attributes:['userId',[Sequelize.fn('SUM', Sequelize.col('amount')), 'total_amount']],
-        group:['userId'],
-        raw:true,
-        order: [
-        // ✅ CORRECT WAY: Use Sequelize.literal to reference the aggregate alias
-        [Sequelize.literal('total_amount'), 'DESC'] 
-    ]
-    })
-    console.log('list of expenses users>>>',userexpenses)
-    const users=await userModel.findAll({raw:true})
-    console.log(users)
-    for(let userexpense of userexpenses){
-        for(let user of users){
-            if(user.id===userexpense.userId){
-                userexpense.name=user.name
-            }
-        }
-    }
+        const userexpenses=await users.findAll({
+            attributes:['id','name',[Sequelize.fn('SUM',Sequelize.col('expenses.amount')),'total_amount']],
+            include:[{
+                model:expenses,
+                attributes:[]
+            }],
+            group:['users.id'],
+            order:[['total_amount','DESC']],
+            raw:true
+
+        })
     console.log("userexpenses>>>",userexpenses)
     res.status(200).json({userexpenses})
 
 
     }
     catch(err){
+        console.log(err)
         res.status(500).json({error:err})
     }
     
