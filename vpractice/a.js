c3ac63afcfc54118e9a329f961575dcefe295291
divyaelagandula <divyaelagandula9014@gmail.com>
2025-10-29 22:32:28 +0530

    added download functionality

--- vpractice/a.js
+++ vpractice/a.js
@@ -8,14 +8,21 @@ const paymentroutes=require('./routes/paymentroutes')
 const premiumroutes=require('./routes/premiumroutes')
 const resetPasswordRoutes = require('./routes/restpasswd')
 const db = require('./utilss/db-connection'); 
+const fs=require('fs')
+const https=require('https')
 const cors = require('cors');
 const path=require('path')
+  const compression = require('compression');
+
 
 const app = express();
 
 
 app.use(express.json()); 
 app.use(cors());
+
+    app.use(compression());
+
 // 1. Static File Serving (Crucial for opening login.html)
 // This serves all files (HTML, CSS, JS) from the 'views' directory automatically.
 // This allows your <a href="/login.html"> link to work without an explicit route.
@@ -32,11 +39,12 @@ app.get('/',(req,res)=>{
 
 
 
-
 db.sync().then(() => {
     console.log('Database synchronized successfully');
+    
+    // Use https.createServer() instead of app.listen()
     app.listen(3000, () => {
-        console.log("Server is running on http://localhost:3000");
+        console.log("Server is running securely on http://localhost:3000");
     });
 }).catch(err => {
     console.error('Failed to synchronize database:', err);
